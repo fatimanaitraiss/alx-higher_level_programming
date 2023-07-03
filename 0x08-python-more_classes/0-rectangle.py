8fa3227f6c0935e204ef087295718fef7b2c28b6
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-07-03 08:46:18 -0700

    hggg

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -7,3 +7,4 @@ Defines an empty class Rectangle
 class Rectangle:
     """Empty representation of a rectangle"""
     pass
+
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -38,3 +38,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
