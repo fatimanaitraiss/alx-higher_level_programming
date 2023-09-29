d17e64e0e9fb92b2f919b15752826fcd9a4c4d65
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-09-29 17:19:48 +0000

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,6 +3,7 @@
 Python script that takes your Github credentials (username and password)
 and uses the Github API to display your id
 """
+import sys
 import requests
 from requests.auth import HTTPBasicAuth
 from sys import argv
