ec09760496b551ce40bea196ecc17b3acc95b3f3
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-09-29 19:40:19 +0000

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,13 +3,41 @@
 Python script that takes your Github credentials (username and password)
 and uses the Github API to display your id
 """
-import sys
 import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+def get_github_user_id(username, personal_access_token):
+    # Define the GitHub API URL for the authenticated user
+    api_url = f'https://api.github.com/user'
+
+    # Create a Basic Authentication header with your username and personal access token
+    auth_header = (username, personal_access_token)
+
+    try:
+        # Send a GET request to the GitHub API with Basic Authentication
+        response = requests.get(api_url, auth=auth_header)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            user_data = response.json()
+            user_id = user_data.get('id')
+            if user_id:
+                return user_id
+            else:
+                print("Failed to retrieve user ID.")
+        else:
+            print(f"Request failed with status code {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <username> <personal_access_token>")
+    else:
+        username = sys.argv[1]
+        personal_access_token = sys.argv[2]
+        user_id = get_github_user_id(username, personal_access_token)
+        if user_id:
+            print(f"Your GitHub User ID is: {user_id}")
+
